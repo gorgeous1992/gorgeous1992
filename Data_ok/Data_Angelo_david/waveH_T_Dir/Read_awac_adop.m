759ec8c7ab0b6edd8720bff6572ed387a2436e09
Zheming Gao <zgao5@ncsu.edu>
2017-07-23 11:14:25 -0400

    add data

---
+++ Data_ok/Data_Angelo_david/waveH_T_Dir/Read_awac_adop.m
@@ -0,0 +1,25 @@
+function [wavehsid, wavetpid, depthid, datatime] = Read_awac_adop(file, date)
+%Input: file on FRF, datatime is date [datenum()]
+%output: wavehight, wave Time period, depth, datestring
+
+
+%% save data file
+%Get time from it.
+tt = ncread(file,'time');
+time = tt/(24*3600) + datenum(1970,1,1);
+tm = date;
+id =find(floor(time) == tm);
+datatime = string(datestr(time(id)));
+
+%ncdisp(file)
+%get wave height
+waveHs = ncread(file, 'waveHs');
+%get wave Time period
+waveTp = ncread(file, 'waveTp');
+%get wave depth
+depth = ncread(file, 'depth');
+
+wavehsid = waveHs(id);
+wavetpid = waveTp(id);
+depthid = depth(id);
+end
