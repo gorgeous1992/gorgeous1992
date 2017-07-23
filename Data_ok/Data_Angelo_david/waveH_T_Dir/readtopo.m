d46ec64426321fb0b1e78972027d8ca694978cb9
Zheming Gao <zgao5@ncsu.edu>
2017-07-23 15:21:08 -0400

    add files

--- Data_ok/Data_Angelo_david/waveH_T_Dir/readtopo.m
+++ Data_ok/Data_Angelo_david/waveH_T_Dir/readtopo.m
@@ -2,12 +2,12 @@ clc
 clear all
 
 %% Import data for wave height
-T = importdata('topo0527.txt');
+T = importdata('topo0428.txt');
 sz = size(T)
 
 %need topo for fixed y = 956
 yid = find(T(:, 5) == 956);
 
-%data saved as [xcoor, topo_value]
-topo = T(yid, [4, 3]);
-save('topo052716','topo');
+%data saved as [longitude, latitude, xcoor, topo_value]
+topo = T(yid, [1, 2, 4, 3]);
+save('topo0428_global','topo');
