ef7a6249eee5252b0d9e1f02e977a1ddbdba20e3
Zheming Gao <zgao5@ncsu.edu>
2017-07-24 14:52:39 -0400

    add files

--- Data_ok/Data041117/3days/Readwavedata.m
+++ Data_ok/Data041117/3days/Readwavedata.m
@@ -12,7 +12,7 @@ xp150 = 'https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/oceanography/waves/x
 xp125 = 'https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/oceanography/waves/xp125m_tjh/2017/FRF-ocean_waves_xp125m_201704.nc';
 %xp200 = 'https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/oceanography/waves/xp200m_tjh/2016/FRF-ocean_waves_xp200m_201704.nc';
 
-DateString = [[2017,04,11,13,00,00];[2017,04,14,13,00,00]];
+DateString = [[2017,04,11,12,00,00];[2017,04,14,13,00,00]];
 dateselected = datenum(DateString);
 %% save data 8m_array 
 
@@ -30,9 +30,9 @@ wave_awac6 = [hs, tp, dp, dir, date];
 wave_awac6(:, 5)
 
 %lack of 04/12/2017 6am,7am
-wave_awac6_3days = str2double([wave_awac6(1:17, [1,2,3,4]);
+wave_awac6_3days = str2double([wave_awac6(1:18, [1,2,3,4]);
                               zeros(2, 4);
-                              wave_awac6(18:end, [1,2,3,4])]);
+                              wave_awac6(19:end, [1,2,3,4])]);
 %% save data  awac_45
 
 [hs, tp, dp, date] = Read_awac_adop(awac_45, dateselected);
@@ -41,7 +41,7 @@ wave_awac45 = [hs, tp, dp, dir, date];
 wave_awac45(:, 5)
 
 %lack of 04/11/2017 13-15pm
-wave_awac45_3days = str2double([zeros(3, 4);
+wave_awac45_3days = str2double([zeros(4, 4);
                                wave_awac45(:, [1,2,3,4])]);
 %% save data  adop_35
 [hs, tp, dp, date] = Read_awac_adop(adop_35, dateselected);
--- Data_ok/Data041117/Plot_topo.m
+++ Data_ok/Data041117/Plot_topo.m
@@ -1,26 +1,29 @@
 clc 
 clear all
-%% Plot wave Height
-%Load Height data
+%% Plot topo 2-D graph
+%Load topo data
 topo = importdata('topo04112017.txt')
-load('topo041117.mat', 'T')
-size(T)
-%Height row is Depth on [x, y].
-%Coordinate x is 50:12:950,
-%Coordinate y is -100:24:1100
-Topo = reshape(T(:, 4), [76, 51]);
-Topo_y1 = T([1:76], 3);
+%topo values
+T = topo(:, 3);
 x_topo = [50:12:950]';
 y_topo = [-100:24:1100]';
 
-[X, Y] = meshgrid(x_topo, y_topo);
-
 figure
+subplot(1,2,1)
+[X, Y] = meshgrid(x_topo, y_topo);
 surf(X, Y, Topo')
 xlabel('Crossshore distance(m)')
 ylabel('Alongshore distance(m)')
 zlabel('Elevation')
 title('Elevation')
+%Height row is Depth on [x, y].
+%Coordinate x is 50:12:950,
+%Coordinate y is -100:24:1100
+
+%load topo data for y=950
+T1 = load('topo401117.mat');
+Topo = (T1(:, 4));
+Topo_y1 = T1(:,3);
 
 figure
 %Plot the depth_y1, one piece
