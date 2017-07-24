6dc87992705e6b27baa17fcbab95abb3df03e8af
Zheming Gao <zgao5@ncsu.edu>
2017-07-24 17:48:46 -0400

    add topo data files

--- Data_ok/Data041117/readtopo.m
+++ Data_ok/Data041117/readtopo.m
@@ -2,7 +2,7 @@ clc
 clear all
 
 %% Import data for wave height
-T = importdata('topo04112017.txt');
+T = importdata('topo010517.txt');
 sz = size(T);
 
 %need topo for fixed y = 956
@@ -10,4 +10,3 @@ yid = find(T(:, 5) == 956);
 
 %data saved as [longitude, latitude, xcoor, topo_value]
 topo = T(yid, [1, 2, 4, 3]);
-save('topo041117_global','topo');
