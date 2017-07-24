24a561139d0159d3062f7085c729a40f2f0a0ba4
Zheming Gao <zgao5@ncsu.edu>
2017-07-24 19:23:22 -0400

    add .mat file

--- Data_ok/BiasData/readcBathydata.m
+++ Data_ok/BiasData/readcBathydata.m
@@ -5,18 +5,26 @@ var = 'https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/dat
 %% Get wavenum from cBathy data
 %ncdisp(var)
 
-dateperiod = [datenum(2017, 01, 01), datenum(2017, 04, 28)];
+% dateperiod = [datenum(2017, 04, 11, 12, 00, 00), datenum(2017, 04, 11, 12, 00, 00)];
+% tt = ncread(var,'time');
+% time = tt/(24*3600) + datenum(1970,1,1);
+% tm = dateperiod;
+
+dateperiod = datenum(2017, 04, 11, 11, 59, 00);
 tt = ncread(var,'time');
 time = tt/(24*3600) + datenum(1970,1,1);
 tm = dateperiod;
+
 %get all needed time
-id = find(time >= tm(1) & time <= tm(2)+1);
-datatime = string(datestr(time(id)))
-length(datatime)
+%id = find(time >= tm(1) & time <= tm(2)+1);
+id = find(time == tm);
+datestr(time(id));
 
 wavedepth = ncread(var, 'depthKF', [1,1,min(id)], [inf, inf, length(id)]);
 xm = ncread(var, 'xm');
 ym = ncread(var, 'ym');
 yjd = find(ym == 950);
-cBathy_depth = wavedepth(:, yjd, :)
+
+cBathy_depth = [xm, wavedepth(:, yjd, :)];
+
 %wavedepth_data = [xm, wavedepth_1d];
