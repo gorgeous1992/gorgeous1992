ce45fca53bc53a0e89bca767a07bfb31b85bbd36
Zheming Gao <zgao5@ncsu.edu>
2017-07-25 09:36:11 -0400

    add .mat files

--- Data_ok/BiasData/readcsv.m
+++ Data_ok/BiasData/readcsv.m
@@ -1,11 +1,11 @@
 clc
 clear all
 
- data = importdata('201704tide.csv')
+ data = importdata('201701tide.csv')
  
  timestamps = string(data.textdata(:, 1))
- id = [find(timestamps == "2017-04-11 12:00"), ...
-     find(timestamps == "2017-04-14 12:00")];
+ id = [find(timestamps == "2017-01-01 12:00"), ...
+     find(timestamps == "2017-01-31 20:00")];
  
  forecasttime = timestamps(id(1):id(2))
  
